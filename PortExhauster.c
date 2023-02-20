879d68ab72fafc46b8eac03cc903260fc3a58d85
dj <davejagoda+github@gmail.com>
2023-02-19 18:07:16 -0800

    Add PortExhauster.c

---
+++ PortExhauster.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+int help()
+{
+  printf("options\n");
+  printf("-h: show this output\n");
+  printf("-t: exhaust TCP ports\n");
+  printf("-u: exhaust UDP ports\n");
+  return 0;
+}
+
+int tcp()
+{
+  printf("tcp\n");
+  int count = 0;
+  int status = 0;
+  struct addrinfo hints;
+  struct addrinfo *res;  // will point to the results
+  struct addrinfo *p;    // to walk the results
+
+  memset(&hints, 0, sizeof hints); // make sure the struct is empty
+  hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
+  hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+  hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+
+  if ((status = getaddrinfo(NULL, "0", &hints, &res)) != 0) {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    exit(1);
+  }
+
+  while (status == 0) {
+    for (p = res; p != NULL; p = p->ai_next) {
+      int sockfd = 0;
+      void *addr;
+      char *ipver;
+
+      sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+      status = bind(sockfd, res->ai_addr, res->ai_addrlen);
+      status = listen(sockfd, 0);
+      if (status != 0) {
+        perror("listen");
+      }
+
+      struct sockaddr_in sin;
+      socklen_t len = sizeof(sin);
+      if (getsockname(sockfd, (struct sockaddr *)&sin, &len) == -1)
+        perror("getsockname");
+      else
+        count ++;
+        if (p->ai_family == AF_INET) { // IPv4
+          ipver = "IPv4";
+        } else { // IPv6
+          ipver = "IPv6";
+        }
+      printf("%s port number %d\n", ipver, ntohs(sin.sin_port));
+    }
+  }
+  printf("Opened %d sockets\n", count);
+  printf("Press return to continue\n");
+  getchar();
+  freeaddrinfo(res); // free the linked-list
+}
+
+int udp()
+{
+  printf("udp\n");
+  int count = 0;
+  int status = 0;
+  struct addrinfo hints;
+  struct addrinfo *res;  // will point to the results
+  struct addrinfo *p;    // to walk the results
+
+  memset(&hints, 0, sizeof hints); // make sure the struct is empty
+  hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
+  hints.ai_socktype = SOCK_DGRAM;  // UDP datagram sockets
+  hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+
+  if ((status = getaddrinfo(NULL, "0", &hints, &res)) != 0) {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    exit(1);
+  }
+
+  while (status == 0) {
+    for (p = res; p != NULL; p = p->ai_next) {
+      int sockfd = 0;
+      void *addr;
+      char *ipver;
+
+      sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+      status = bind(sockfd, res->ai_addr, res->ai_addrlen);
+
+      struct sockaddr_in sin;
+      socklen_t len = sizeof(sin);
+      if (getsockname(sockfd, (struct sockaddr *)&sin, &len) == -1)
+        perror("getsockname");
+      else
+        count ++;
+        if (p->ai_family == AF_INET) { // IPv4
+          ipver = "IPv4";
+        } else { // IPv6
+          ipver = "IPv6";
+        }
+      printf("%s port number %d\n", ipver, ntohs(sin.sin_port));
+    }
+  }
+  printf("Opened %d sockets\n", count);
+  printf("Press return to continue\n");
+  getchar();
+  freeaddrinfo(res); // free the linked-list
+}
+
+int main (int argc, char **argv)
+{
+  int c;
+
+  while ((c = getopt (argc, argv, "htu")) != -1)
+    switch (c)
+      {
+      case 'h':
+        help();
+        break;
+      case 't':
+        tcp();
+        break;
+      case 'u':
+        udp();
+        break;
+      }
+  return 0;
+}
